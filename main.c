a71fc02d5f8011f11e6a7e09487484d69c522d23
Joseph Bautista <josephbautista@Josephs-MacBook-Pro.local>
2022-05-28 02:54:30 +0800

    added while loop cli

--- main.c
+++ main.c
@@ -27,31 +27,27 @@ int evaluate(ParseObject *object) {
 }
 
 int main() {
-  int num_tokens = 0;
-  // char *stream = "1 + 3 - 4 + 100 - 99";
-  char *stream = "(12 + 12) - 6 * 4";
-  int stream_length = strlen(stream);
-  Token **tokens = lexer(stream, stream_length, &num_tokens);
-  ParseObject *ast = parse(tokens, num_tokens);
-  int evaluated_value = evaluate(ast);
-  printf("evaluated_value: %d\n", evaluated_value);
+  char message[BUFFER_SIZE];
+  while (true) {
+    printf("Math >  ");
+    fgets(message, BUFFER_SIZE, stdin);
+    int stream_length = strlen(message);
+    message[stream_length - 1] = '\0'; // terminate string early
 
+    char *stream = malloc(stream_length * sizeof(char));
+    memcpy(stream, message, stream_length);
 
-  // char message[BUFFER_SIZE];
-  // while (true) {
-  //   printf("Math >  ");
-  //   fgets(message, BUFFER_SIZE, stdin);
-  //   int stream_length = strlen(message);
-  //   message[stream_length - 1] = '\0'; // terminate string early
+    if (strcmp(stream, "exit") == 0) {
+      printf("Exiting program...\n");
+      return 1;
+    }
 
-  //   char *stream = malloc(stream_length * sizeof(char));
-  //   memcpy(stream, message, stream_length);
+    int size = 0;
+    Token **tokens = lexer(stream, stream_length, &size);
+    ParseObject *ast = parse(tokens, size);
+    int evaluated_value = evaluate(ast);
+    printf("%d\n", evaluated_value);
 
-  //   int size = 0;
-  //   Token **tokens = lexer(stream, stream_length, &size);
-  //   for (int idx = 0; idx < size; idx++) {
-  //     printf("%s %s\n", tokens[idx]->type, tokens[idx]->value);  
-  //   }
-  // }
+  }
   return 0;
 }
